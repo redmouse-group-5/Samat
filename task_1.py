0e385a1c05187d554f4261a74ad6ad07c5374e1f
ovuzu747 <samatmambetshaev@gmail.com>
2016-12-17 14:19:05 +0600

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,20 +1,20 @@
-x = int(input ("Введите число от 1 до 9: "))
-if x>=1 and x<=3:
-    s = str(input("Введите строку: "))
-    n = int(input("Введите кол-во повторов строки: "))
-    i = 0
-    while i < n:
-        print (s)
-        i = i+1
-        
-elif x>=4 and x <=6:
-    m = int(input("Введите степень, в которую следует возвести число: "))
-    print (x**m)
-elif x >= 7 and x <=9:
-    i = 0
-    while i <10:
-            x = x+1
-            i = i+1
-            print (x)
+x=int(input("Введите число от 1 до 9:"))
+if 1<=x<=3:
+    s=str(input("Введите строку:"))
+    n=int(input("Введите кол-во повторов строки:"))
+    i=0
+    while i<n:
+        print(s)
+        i=i+1        
+elif 4<=x<=6:
+    m=int(input("Введите степень, в которую следует возвести число:"))
+    print(x**m)
+
+elif 7<=x<=9:
+    i=0
+    while i<10:
+        x=x+1
+        i=i+1
+        print(x)
 else:
     print("Ошибка ввода")
